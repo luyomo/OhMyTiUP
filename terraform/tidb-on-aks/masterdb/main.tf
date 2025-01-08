bf5e5c12ab19e18b0b1bbd6fd0e7b3d8ed1aad31
luyomo <syunka.tyo@gmail.com>
2025-01-08 00:20:41 +0000

    Improved the tidb on aks deployment

--- terraform/tidb-on-aks/masterdb/main.tf
+++ terraform/tidb-on-aks/masterdb/main.tf
@@ -4,14 +4,15 @@ variable "register_app_client_secret" {}
 module "masteraks" {
     source = "../modules/aks"
 
-    cluster_name = "jaytest001"
-    k8s_version = "1.25.11"
-    ticdc_node_count = 3
-    kube_config_file = "/tmp/master_kubeconfig"
-    cluster_version = "v7.1.1"
+    cluster_name = "tidb-on-aks-demo"                                  # Cluster Name
+    k8s_version = "1.30.0"                                             # K8S version
+#    ticdc_node_count = 3                                              # Number TiCDC nodes
+    kube_config_file = "/tmp/master_kubeconfig"                        # K8S Config file 
+    cluster_version = "v7.5.2"                                         # TiDB Version
 
-    register_app_client_id = "${var.register_app_client_id}"
-    register_app_client_secret = "${var.register_app_client_secret}"
+    register_app_client_id = "${var.register_app_client_id}"           # Azure APP Registration client id
+    register_app_client_secret = "${var.register_app_client_secret}"   # Azure APP Registration secret id
+    resource_group = "jp-presale-test"                                 # resource group name in which AKS is created
 }
 
 module "mastertidb" {
@@ -19,15 +20,15 @@ module "mastertidb" {
 
     source = "../modules/tidb-operator"
 
-    cluster_name = "jaytest001"
-    k8s_version = "1.25.11"
-    ticdc_node_count = 3
+    cluster_name = "tidb-on-aks-demo"                                  # Cluster Name 
+    k8s_version = "1.30.0"                                             # K8S version
+#    ticdc_node_count = 3                                              # Number of TiCDC nodes
     kube_config_file = "/tmp/master_kubeconfig"
+    cluster_version = "v7.5.2"                                         # TiDB Version
     kube_config      = module.masteraks.kube_config
     cluster_ca_cert = module.masteraks.cluster_ca_certificate
     client_certificate = module.masteraks.client_certificate
     client_key = module.masteraks.client_key
     cluster_endpoint = module.masteraks.host
-    cluster_version = "v7.1.1"
 
 }
